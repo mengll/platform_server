31bb949d65108d43cdb4a5db87a6f06197da00e5
wuzekang <zekang.wu@gmail.com>
2018-04-18 12:49:37 +0800

    add bottle flip game

--- client/src/containers/play/index.jsx
+++ client/src/containers/play/index.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import client from  '../../client';
+import { AuthContext } from '../../context';
 
 class Play extends Component {
     state= {
@@ -11,25 +12,46 @@ class Play extends Component {
     params = null;
 
     handleClick = () => {
-        const score = this.state.score;
-        client.call('room_message',  {room: this.params.room, game_id: "1990", data:{score} })
+        const { profile } = this.props;
+        const score = this.state.score + 1;
         this.setState({
-            score: score + 1
+            score: score
         })
+        client.call('room_message',  {room: this.params.room, data:{score, uid: profile.uid} })
     }
 
     componentDidMount() {
-        client.on('notify.room_message', (params) => {
-            this.setState({
-                other: params.data.score
-            })
-        })
-        const params = this.props.location.state;
+        const { profile, params } = this.props;
         this.params = params;
+
+        client.on('notify.room_message', ({data}) => {
+            if (data.uid !== profile.uid) {
+                this.setState({
+                    other: data.score
+                })
+            }
+        })
+
+        setTimeout(() => {
+            client.call('game_result')            
+        }, 10000);
+        
+        
     }
 
     render() {
+        return <div onClick={this.handleClick}>{this.state.score} - {this.state.other}</div>;
+    }
+}
 
-        return <div onClick={this.handleClick}>{this.state.score}-{this.state.other}</div>;
+
+export default class Wrapper extends Component {
+    render() {
+        const params = this.props.location.state;
+        return <AuthContext.Consumer>
+          {
+              ({profile}) => <Play profile={profile} params={params}/>
+          }  
+        </AuthContext.Consumer>;
     }
 }
