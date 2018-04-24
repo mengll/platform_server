0a4f208486408b582b58c7e8d76a88b2549d8371
mengll <mll@anfeng.com>
2018-04-24 10:54:50 +0800

    debug

--- server/gameserver.go
+++ server/gameserver.go
@@ -258,7 +258,7 @@ func Gs(ws *websocket.Conn, req_data *ReqDat) error {
 
 
 		back_dat := make(map[string]interface{})
-		back_dat["online_num"] = login_num + 1
+		back_dat["online_num"] = login_num
 		back_dat["game_id"] = game_id
 
 		//返回登录
